d289b41c31458938cbefb65d4363eeeb5b6bfba5
keremad <kerem@alrahman.de>
2016-05-13 14:27:07 +0200

    Performance: Use the ViewHolder pattern to avoid repeated resource lookups.

--- app/src/main/java/ch/sbb/adiguzaf/rssreader/activity/RSSCursorAdapter.java
+++ app/src/main/java/ch/sbb/adiguzaf/rssreader/activity/RSSCursorAdapter.java
@@ -11,16 +11,23 @@ import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.Locale;
 
 import ch.sbb.adiguzaf.rssreader.provider.FeedsContract;
 
 import static ch.sbb.adiguzaf.rssreader.R.id;
 import static ch.sbb.adiguzaf.rssreader.R.layout;
+import static ch.sbb.adiguzaf.rssreader.R.string;
 
 class RSSCursorAdapter extends CursorAdapter {
 
     private static final String mName = RSSCursorAdapter.class.getSimpleName();
+    private DateFormat dateFormat = DateFormat.getDateTimeInstance();
+
+    // the ViewHolder pattern is used to avoid repeated resource lookups
+    private static class ViewHolder {
+        TextView titleText;
+        TextView contentText;
+    }
 
     public RSSCursorAdapter(Context context) {
         super(context, null, 0);
@@ -28,30 +35,36 @@ class RSSCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(layout.item_feeds, parent, false);
+        final View view = LayoutInflater.from(context).inflate(layout.item_feeds, parent, false);
+        ViewHolder viewHolder = new ViewHolder();
+        viewHolder.titleText = (TextView) view.findViewById(id.feeds_title);
+        viewHolder.contentText = (TextView) view.findViewById(id.feeds_content);
+        view.setTag(viewHolder);
+        return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        int descIndex = cursor.getColumnIndex(FeedsContract.FEEDS_COLUMN_DESCRIPTION);
-        int titleIndex = cursor.getColumnIndex(FeedsContract.FEEDS_COLUMN_TITLE);
-        int pubDateIndex = cursor.getColumnIndex(FeedsContract.FEEDS_COLUMN_PUBLISHED_DATE);
+        int descIndex = cursor.getColumnIndexOrThrow(FeedsContract.FEEDS_COLUMN_DESCRIPTION);
+        int titleIndex = cursor.getColumnIndexOrThrow(FeedsContract.FEEDS_COLUMN_TITLE);
+        int pubDateIndex = cursor.getColumnIndexOrThrow(FeedsContract.FEEDS_COLUMN_PUBLISHED_DATE);
 
         String title = cursor.getString(titleIndex);
+        String pubDate = getPubDate(cursor, pubDateIndex);
+        String text = title + "\n" +
+                context.getString(string.published_on) + pubDate;
         String description = cursor.getString(descIndex);
 
-        Calendar cal = Calendar.getInstance(Locale.GERMAN);
-        cal.setTimeInMillis(cursor.getLong(pubDateIndex));
-        DateFormat dateFormat = DateFormat.getDateTimeInstance();
-        String pubDate = dateFormat.format(cal.getTime());
-
-        // TODO: use the ViewHolder pattern for performance
-        TextView titleText = (TextView) view.findViewById(id.feeds_title);
-        String text = title + ", erschienen am " + pubDate + "\n";
-        titleText.setText(text);
-        TextView contentText = (TextView) view.findViewById(id.feeds_content);
-        contentText.setText(description);
+        final ViewHolder viewHolder = (ViewHolder) view.getTag();
+        viewHolder.titleText.setText(text);
+        viewHolder.contentText.setText(description);
 
         Log.d(mName, "Content bound");
+    }
+
+    private String getPubDate(Cursor cursor, int pubDateIndex) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(cursor.getLong(pubDateIndex));
+        return dateFormat.format(cal.getTime());
     }
 }
